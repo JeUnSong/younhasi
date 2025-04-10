c0cf2aad192950e3e3a0beeb23881d39c32590ea
jaeunsong <jaeunsong.dev@gmail.com>
2025-04-10 14:29:00 +0900

    refactor: 푸터 구성 수정

--- younha-fan-community-front/src/components/Footer.tsx
+++ younha-fan-community-front/src/components/Footer.tsx
@@ -16,8 +16,11 @@ export default function Footer() {
                 © 2025 YOUNHASI | Made with love at 4:29<span className="text-accent text-[13px] align-middle ml-1">♥</span>
             </div>
             <div className="space-x-4 text-[11px] text-neutral-400">
-                <a href="#" className="hover:underline">이용약관</a>
-                <a href="#" className="hover:underline">개인정보처리방침</a>
+                <a href="https://www.instagram.com/younha_holic/" className="hover:underline" target="_blank">Instagram</a>
+                <a className="hover:underline">|</a>
+                <a href="https://www.youtube.com/@YOUNHAOFFICIAL" className="hover:underline" target="_blank">YouTube</a>
+                <a className="hover:underline">|</a>
+                <a href="https://x.com/younhaholic" className="hover:underline" target="_blank">Twitter</a>
             </div>
         </footer>
     );
